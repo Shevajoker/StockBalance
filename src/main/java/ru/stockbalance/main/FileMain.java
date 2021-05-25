fdfc159c281709ab5fae22d84adabe03776e75cd
Shevajoker <Shevajoker@gmail.com>
2021-05-25 16:26:36 +0300

    no change

--- src/main/java/ru/stockbalance/main/FileMain.java
+++ src/main/java/ru/stockbalance/main/FileMain.java
@@ -12,8 +12,8 @@ public class FileMain {
 	public static void main(String[] args) throws IOException {
 		
 		FileStock file = new FileStock();
-//		String fileDir = "E:\\24 05 2021\\ost.xls";
-		String fileDir =  "http://77.74.28.4:22181/brw/ostatki.xls";
+		String fileDir = "E:\\24 05 2021\\ost.xls";
+//		String fileDir =  "http://77.74.28.4:22181/brw/ostatki.xls";
 		
 //		URL url = new URL("http://77.74.28.4:22181/brw/ostatki.xls");
 //		String fileDir = url.getFile();
