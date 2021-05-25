6a1d558c472c6546ff93365de1f951726c48d440
unknown <C:\Users\Designer\AppData\Roaming\The Bat!>
2021-05-25 11:18:29 +0300

    add Articles table

---
+++ src/main/java/ru/stockbalance/model/Article.java
@@ -0,0 +1,83 @@
+package ru.stockbalance.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+/**
+ * 77.74.28.4:22181/brw/ostatki.xls
+ * @author Designer
+ *
+ */
+@Entity
+public class Article {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String article;
+	private String name;
+	private int stock;
+	private Date date;
+	
+	public Article() {}
+	
+	public Article(String article, String name, Date date, int stock) {
+		this.article = article;
+		this.name = name;
+		this.date = date;
+		this.stock = stock;
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+	public String getArticle() {
+		return article;
+	}
+
+	public void setArticle(String article) {
+		this.article = article;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", article=" + article + ", name=" + name + ", stock=" + stock + "]";
+	}
+
+	
+	
+	
+}
--- src/main/webapp/stocktable.jsp
+++ src/main/webapp/stocktable.jsp
@@ -11,5 +11,29 @@
 <h3>Hello ${login}</h3>
 
 
+<br>
+<input type="date" value="2020-05-25">
+
+<br>
+<br>
+
+
+<table border="1" >
+<tr>
+	<td>Name</td>
+	<td>date1</td>
+	<td>date2</td>
+</tr>
+<tr>
+	<td>bead</td>
+	<td>20</td> 
+	<td>10</td>
+</tr>
+
+
+</table>
+
+
+
 </body>
 </html>
